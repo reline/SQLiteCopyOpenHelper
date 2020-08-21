2a8bd53d122b6907b2fb0bc84648b19d6b7bc9ec
Nathan Reline <nathanielreline@gmail.com>
2020-08-20 21:17:57 -0700

    Flesh out low-level implementation

---
+++ example/src/main/java/com/github/reline/example/FeedReaderDbHelper.kt
@@ -0,0 +1,59 @@
+package com.github.reline.example
+
+import android.content.Context
+import android.provider.BaseColumns
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.sqlite.db.SupportSQLiteOpenHelper
+import net.sqlcipher.database.SupportFactory
+
+object FeedReaderContract {
+    // Table contents are grouped together in an anonymous object.
+    object FeedEntry : BaseColumns {
+        const val TABLE_NAME = "entry"
+        const val COLUMN_NAME_TITLE = "title"
+        const val COLUMN_NAME_SUBTITLE = "subtitle"
+    }
+}
+
+private const val SQL_CREATE_ENTRIES =
+    "CREATE TABLE ${FeedReaderContract.FeedEntry.TABLE_NAME} (" +
+            "${BaseColumns._ID} INTEGER PRIMARY KEY," +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE} TEXT," +
+            "${FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE} TEXT)"
+
+private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${FeedReaderContract.FeedEntry.TABLE_NAME}"
+
+class FeedReaderDbFactory(
+    private val context: Context,
+    private val password: ByteArray
+) {
+    fun create(): SupportSQLiteOpenHelper {
+        val config = SupportSQLiteOpenHelper.Configuration.builder(context)
+            .callback(FeedReaderDbHelperCallback(FeedReaderDbHelperCallback.DATABASE_VERSION))
+            .name(FeedReaderDbHelperCallback.DATABASE_NAME)
+            .build()
+
+        return SupportFactory(password).create(config)
+    }
+}
+
+class FeedReaderDbHelperCallback(version: Int) : SupportSQLiteOpenHelper.Callback(version) {
+    override fun onCreate(db: SupportSQLiteDatabase) {
+        db.execSQL(SQL_CREATE_ENTRIES)
+    }
+    override fun onUpgrade(db: SupportSQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // This database is only a cache for online data, so its upgrade policy is
+        // to simply to discard the data and start over
+        db.execSQL(SQL_DELETE_ENTRIES)
+        onCreate(db)
+    }
+    override fun onDowngrade(db: SupportSQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        onUpgrade(db, oldVersion, newVersion)
+    }
+
+    companion object {
+        // If you change the database schema, you must increment the database version.
+        const val DATABASE_VERSION = 1
+        const val DATABASE_NAME = "FeedReader.db"
+    }
+}
